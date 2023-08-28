016556cf93f9497fa9de4e25bb10b19de0052ae5
Toso1 <142443855+Toso1@users.noreply.github.com>
2023-08-28 17:30:17 -0300

    First task codes

--- pointwise/PointwiseTransform.java
+++ pointwise/PointwiseTransform.java
@@ -1,4 +1,3 @@
-
 public class PointwiseTransform extends Object {
 
 	/**
@@ -27,7 +26,15 @@ public class PointwiseTransform extends Object {
 		double max = input.getMaximum();
 		double min = input.getMinimum();
 		ImageAccess output = new ImageAccess(nx, ny);
-		// Add your code here
+        double alpha = 255 / (max - min);
+        double beta = min;
+		double value = 0.0;
+		for (int x=0; x<nx; x++)
+		for (int y=0; y<ny; y++) {
+			value = alpha * (input.getPixel(x, y) - beta);
+			output.putPixel(x, y, value);
+        }
+    
 		return output;	
 	}
 
@@ -38,7 +45,16 @@ public class PointwiseTransform extends Object {
 		int nx = input.getWidth();
 		int ny = input.getHeight();
 		ImageAccess output = new ImageAccess(nx, ny);
-		// Add your code here
+		double value = 0.0;
+		for (int x=0; x<nx; x++)
+		for (int y=0; y<ny; y++) {
+			value = input.getPixel(x, y);
+            if (value > 10000)
+                {output.putPixel(x, y, 10000);}
+            else{output.putPixel(x, y, value);}
+       
+  
+		}
 		return output;
 	}
 	
@@ -50,20 +66,40 @@ public class PointwiseTransform extends Object {
 		int ny = zstack[0].getHeight();
 		int nz = zstack.length;
 		ImageAccess output = new ImageAccess(nx, ny);
-		// Add your code here
+		double value_max = 0.0;
+		for (int x=0; x<nx; x++)
+		for (int y=0; y<ny; y++) {
+			value_max= zstack[0].getPixel(x, y);
+			for (int z=0; z<nz; z++) {
+            	if (value_max < zstack[z].getPixel(x, y))
+
+                {value_max = zstack[z].getPixel(x, y);}
+			}
+			output.putPixel(x, y, value_max);
+		}
 		return output;	
 	}
-
 	/**
 	* Question 4.2 Z-stack mean
 	*/
 	static public ImageAccess zprojectMean(ImageAccess[] zstack) {
-		int nx = zstack[0].getWidth();
-		int ny = zstack[0].getHeight();
-		int nz = zstack.length;
-		ImageAccess output = new ImageAccess(nx, ny);
-		// Add your code here
-		return output;	
-	}
+	    int nx = zstack[0].getWidth();
+	    int ny = zstack[0].getHeight();
+	    int nz = zstack.length;
+	    ImageAccess output = new ImageAccess(nx, ny);
+		double value_sum = 0.0;
+	    for (int x = 0; x < nx; x++) {
+	        for (int y = 0; y < ny; y++) {
+	            value_sum = 0.0;  // Reset value_sum for each (x, y) position
+	            for (int z = 0; z < nz; z++) {
+	                value_sum += zstack[z].getPixel(x, y);
+	            }
+	            output.putPixel(x, y, value_sum / nz);
+	        }
+	    }
+
+	    return output;
+	}		
 
 }
+
